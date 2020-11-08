722c1938d04fd89de29229d35b3583b517da1001
antonpanchishko <PanchishkoMayor@gmail.com>
2020-11-08 21:18:54 +0200

    Commit after create new class Animal

---
+++ src/main/java/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    int age;
+    String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        int a = 119;
-        int b = 290;
-        System.out.println(sum(a, b));
-        System.out.println(div(a, b));
-    }
-    public static int sum(int first, int second){
-        return first + second;
-    }
-    public static int div(int first, int second){
-        return first - second;
+        Animal animal  = new Animal();
+        animal.setAge(5);
+        animal.setName("Bobik");
     }
 }
